63d814ce9427b0c18fae3724ec681fbed6061153
Pandapm <panda_m@126.com>
2017-09-21 10:01:15 +0800

    m

--- JavaScript/14-Longest-Common-Prefix.js
+++ JavaScript/14-Longest-Common-Prefix.js
@@ -9,16 +9,12 @@ var longestCommonPrefix = function (strs) {
     }
     const arr = strs[0].split('');
     for (let j = 0; j < arr.length; j++) {
-        let flag = true;
         pre += arr[j];
         for (let i = 0; i < strs.length; i++) {
-            if (strs[i].indexOf(pre) < 0) {
-                flag = false;
+            if (strs[i].indexOf(pre) != 0 ) {
+                pre = pre.substr(0, pre.length -1);
             }
         }
-        if (!flag) {
-            break;
-        }
     }
-    return pre.substr(0, pre.length - 1);
+    return pre;
 }
